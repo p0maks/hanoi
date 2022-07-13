a80a5fa95cdfe3ce45108a460b49d83752011969
p0maks <golosova_v19@mail.ru>
2022-07-13 19:51:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+
+int num = 0;
+
+void Show(char x, char y)
+{
+	num++; 
+	printf("%c->%c\n", x, y);
+}
+
+void Hanoi(int n, char a, char b, char c)
+{
+	if (n == 1)
+	{
+		Show(a, c);
+	}
+	else
+	{
+		Hanoi(n - 1, a, c, b); 
+		Show(a, c); 
+		Hanoi(n - 1, b, a, c); 
+	}
+}
+
+int main()
+{
+	Hanoi(4, 'a', 'b', 'c');
+	printf("%d\n", num);
+}
