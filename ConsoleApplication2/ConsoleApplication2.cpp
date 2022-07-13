fe1d1cb783cf382597f65e6e2da16bb84902df5f
p0maks <golosova_v19@mail.ru>
2022-07-13 19:54:43 +0300

    two

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -8,9 +8,23 @@ void Show(char x, char y)
 	num++; 
 	printf("%c->%c\n", x, y);
 }
+void Hanoi(int n, char a, char b, char c)
+{
+	if (n == 1)
+	{
+		Show(a, c);
+	}
+	else
+	{
+		Hanoi(n - 1, a, c, b);
+		Show(a, c);
+		Hanoi(n - 1, b, a, c);
+	}
 
+}
 
 int main()
 {
-	
+	Hanoi(4, 'a', 'b', 'c');
+	printf("%d\n", num);
 }
